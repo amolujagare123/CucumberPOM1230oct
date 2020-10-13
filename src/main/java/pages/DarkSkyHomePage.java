7913c1211e1011aac708bc415fdc692998c2ded7
PC <amolujagare@gmail.com>
2020-10-13 13:34:20 +0530

    Initial commit

---
+++ src/main/java/pages/DarkSkyHomePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class DarkSkyHomePage {
+
+       By currettemp = By.xpath("//span[@class='summary swap']");
+
+       By timelineTemp = By.xpath("//span[@class='first']//span");
+
+
+}
